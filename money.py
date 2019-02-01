68c4221906479cc1760e27d95127f57118143b84
michalmx2 <michalmx2@gmail.com>
2019-02-01 13:53:46 +0100

    second version of money

--- money.py
+++ money.py
@@ -1,5 +1,7 @@
 #its a random program :)
 
+
+'''
 import random
 s = []
 for x in range(7):
@@ -8,4 +10,10 @@ for x in range(7):
 avg = sum(s) / len(s)
 print(s)
 print(sum(s))
-print(avg)
+print(avg)'''
+
+import random
+
+li = [random.randint(1,1001) for x in range(7)]
+print('Here is a list of random numbers between 1 and 1000:', li)
+print('Let\'s print a sum of all numbers: {} and average of all numbers: {}'.format(sum(li), round(sum(li)/len(li),2)))
